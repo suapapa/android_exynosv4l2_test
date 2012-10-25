3df1850f34b7fedb0d2721d24003633efe579643
Homin Lee <suapapa@insignal.co.kr>
2012-10-25 16:35:20 +0900

    Refactored functions to setup media links
    Signed-off-by: Homin Lee <suapapa@insignal.co.kr>

--- exynosv4l2_test.cpp
+++ exynosv4l2_test.cpp
@@ -2,28 +2,38 @@
 #define LOG_TAG "exynosv4l2_test"
 #include <cutils/log.h>
 
-#include <exynos_v4l2.h>
+#include <fcntl.h>
 
+#include <exynos_v4l2.h>
 
 #define DEV_MEDIA "/dev/media1"
 #define ME_NAME_SENSOR "mt9p111"
 #define ME_NAME_FLITE_SD "flite-subdev.0"
 #define ME_NAME_FLITE_VD "exynos-fimc-lite.0"
 
-inline static media_entity* _mcGetEntity(media_device* m, char* n)
+static int _mcSetupLink(media_device* m, const char* src, const char* snk)
 {
-    return exynos_media_get_entity_by_name(m, n, strlen(n));
-}
+    ALOGI("Setup link: %s->%s", src, snk);
 
-static int _mcSetupLink(media_device* m, media_entity* src, media_entity* sink)
-{
-    for (unsigned int i = 0; i < src->num_links; i++) {
-        media_link* link = &(src->links[i]);
-	media_pad *srcPad = link->source;
-	media_pad *sinkPad = link->sink;
+    media_entity* meSrc = exynos_media_get_entity_by_name(m, src, strlen(src));
+    if (meSrc == NULL) {
+        ALOGE("Failed to get media entities for %s!", src);
+        return -1;
+    }
 
-        if (link && srcPad->entity == src && sinkPad->entity == sink) {
-            return exynos_media_setup_link(m, srcPad, sinkPad,
+    media_entity* meSnk = exynos_media_get_entity_by_name(m, snk, strlen(snk));
+    if (meSnk == NULL) {
+        ALOGE("Failed to get media entities for %s!", snk);
+        return -1;
+    }
+
+    for (unsigned int i = 0; i < meSrc->num_links; i++) {
+        media_link* link = &(meSrc->links[i]);
+        media_pad* srcPad = link->source;
+        media_pad* snkPad = link->sink;
+
+        if (srcPad->entity == meSrc && snkPad->entity == meSnk) {
+            return exynos_media_setup_link(m, srcPad, snkPad,
                                            MEDIA_LNK_FL_ENABLED);
         }
     }
@@ -32,29 +42,46 @@ static int _mcSetupLink(media_device* m, media_entity* src, media_entity* sink)
     return -1;
 }
 
-static int mcSetupLinks(void)
+static int setupMediaLinksForCamera(void)
 {
     media_device* md = exynos_media_open(DEV_MEDIA);
     if (md == NULL) {
-        ALOGE("failed to open %s", DEV_MEDIA);
+        ALOGE("failed to open %s!", DEV_MEDIA);
         return -1;
     }
 
-    media_entity* meSensor = _mcGetEntity(md, ME_NAME_SENSOR);
-    media_entity* meFliteSd = _mcGetEntity(md, ME_NAME_FLITE_SD);
-    media_entity* meFliteVd = _mcGetEntity(md, ME_NAME_FLITE_VD);
-
-    mcSetupLink(md, meSensor, meFliteSd);
-    mcSetupLink(md, meFliteSd, meFliteVd);
+    _mcSetupLink(md, ME_NAME_SENSOR, ME_NAME_FLITE_SD);
+    _mcSetupLink(md, ME_NAME_FLITE_SD, ME_NAME_FLITE_VD);
 
     exynos_media_close(md);
     return 0;
+}
+
+static int openCamera(int ch)
+{
+    int fd = exynos_v4l2_open_devname(ME_NAME_FLITE_VD, O_RDWR);
+    if (0 > fd) {
+        ALOGE("Failed to open video dev. for %s!", ME_NAME_FLITE_VD);
+        return -1;
+    }
+
+    int err = exynos_v4l2_s_input(fd, ch);
+    if (0 > err) {
+        ALOGE("Failed to set input ch. to %d", ch);
+        goto err_out;
+    }
+
+err_out:
+    exynos_v4l2_close(fd);
+    return err;
 }
 
 #define DEV_VD_CAM "dev/video36"
 
 int main(int argc, char* argv[])
 {
-    mcSetupLinks();
+    setupMediaLinksForCamera();
+    openCamera(0);
+
     return 0;
 }
